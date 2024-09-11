a34d54a7f33d49703a159092465f9b382926405f
Daniel Leiva <ignacioleiva79@gmail.com>
2024-09-11 16:34:13 -0400

    html

--- N1.php
+++ N1.php
@@ -1,4 +1,15 @@
 <?php
-echo 'hola?
+echo 'hola';
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    mas de lo mismo
+</body>
+</html>
